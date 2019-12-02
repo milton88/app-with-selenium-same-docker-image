26f0fa28dd9bcad30a948c722fddf64ca1a793d0
Milton César Rodrigues <milton.rodrigues@programmers.com.br>
2019-12-02 16:36:29 -0200

    first commit

---
+++ src/POC.Selenium.API/Controllers/JobController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+
+namespace POC.Selenium.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JobController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return Job.Job.Execute();
+        }
+    }
+}
---
+++ src/POC.Selenium.Job/Job.cs
@@ -0,0 +1,28 @@
+﻿using OpenQA.Selenium.Chrome;
+
+namespace POC.Selenium.Job
+{
+    public static class Job
+    {
+        public static string Execute()
+        {
+            //var basePath = System.IO.Directory.GetCurrentDirectory().Substring(0, System.IO.Directory.GetCurrentDirectory().IndexOf("POC.Selenium"));
+            //string driverPath = $"{basePath}POC.Selenium\\src\\POC.Selenium.Job\\WebDrivers";
+
+            string driverPath = $"/app/assets/WebDrivers";
+
+            var chromeOptions = new ChromeOptions();
+            chromeOptions.AddArguments("headless");
+            chromeOptions.AddArguments("--no-sandbox");
+            chromeOptions.AddArguments("--disable-dev-shm-usage");
+
+            using (var browser = new ChromeDriver(driverPath, chromeOptions))
+            {
+                // add your code here
+            }
+
+
+            return "Job 2";
+        }
+    }
+}
